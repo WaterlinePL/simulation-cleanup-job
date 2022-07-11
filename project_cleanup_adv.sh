e17053e260bbd0e3429957d523fcc779e111486e
Observer46 <microplayerpl@gmail.com>
2022-07-11 10:59:11 +0200

    change cleanup script and add advanced version

--- project_cleanup_adv.sh
+++ project_cleanup_adv.sh
@@ -5,7 +5,13 @@ project_name=$1
 
 mc alias set minio https://$ENDPOINT $ACCESS_KEY $SECRET_KEY --api S3v4
 cd /workspace
-zip -r $project_name.zip $project_name/modflow
+for model in "${@:2}"
+do
+    read -ra model_type_and_name <<< $model
+    model_type=${model_type_and_name[0]}
+    model_name=${model_type_and_name[1]}
+    zip -r $project_name.zip $project_name/$model_type/$model_name
+done
 mc cp /workspace/$project_name.zip minio/output/$project_name.zip
 if [ $? -eq 0 ]; then
     rm -r /workspace/$project_name /workspace/$project_name.zip
